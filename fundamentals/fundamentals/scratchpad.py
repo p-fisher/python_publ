ce2cdc7faef2a1842eadb7cde0e2741ecd0d91f8
p-fisher <ops.patrickfisher@gmail.com>
2022-01-13 12:44:42 -0500

    organizing

--- fundamentals/fundamentals/scratchpad.py
+++ fundamentals/fundamentals/scratchpad.py
@@ -22,3 +22,16 @@ def print_hello_x_or_ten_times(x = 10):
 
 # print_hello_x_or_ten_times()
 print_hello_x_or_ten_times(4)
+
+
+# set defaults when declaring the parameters
+def be_cheerful(name='', repeat=2):
+	print(f"good morning {name}\n" * repeat)
+be_cheerful()    # output: good morning (repeated on 2 lines)
+be_cheerful("tim")   # output: good morning tim (repeated on 2 lines)
+be_cheerful(name="john")    # output: good morning john (repeated on 2 lines)
+be_cheerful(repeat=6)   # output: good morning (repeated on 6 lines)
+be_cheerful(name="michael", repeat=5)   # output: good morning michael (repeated on 5 lines)
+# NOTE: argument order doesn't matter if we are explicit when sending in our arguments!
+be_cheerful(repeat=3, name="kb")    # output: good morning kb (repeated on 3 lines)
+
