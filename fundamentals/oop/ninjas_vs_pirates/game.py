5121ec4cb8f8f188c05a6fe4340ede734210abb5
p-fisher <ops.patrickfisher@gmail.com>
2022-01-23 15:50:31 -0500

    yes

---
+++ fundamentals/oop/ninjas_vs_pirates/game.py
@@ -0,0 +1,15 @@
+from classes.ninja import Ninja
+from classes.pirate import Pirate
+
+michelangelo = Ninja("Michelanglo")
+
+jack_sparrow = Pirate("Jack Sparrow")
+
+michelangelo.attack(jack_sparrow)
+jack_sparrow.show_stats()
+
+michelangelo.attack(jack_sparrow)
+jack_sparrow.show_stats()
+
+michelangelo.attack(jack_sparrow)
+jack_sparrow.show_stats()
